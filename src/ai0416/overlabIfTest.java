d197512afeb08bc24dca0b143bbf979e9d023e5f
2601110286-ai <2601110286@gspace.kopo.ac.kr>
2026-04-16 11:44:44 +0900

    0416

---
+++ src/ai0416/overlabIfTest.java
@@ -0,0 +1,23 @@
+package ai0416;
+
+import java.util.Scanner;
+
+public class overlabIfTest {
+    public static void main(String[] args) {
+        System.out.println("====== 특정 범위 숫자 판별 프로그램 ======");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        if (num > 100) {
+            if (num < 1000) {
+                System.out.printf("입력된 숫자 %d는(은) 100보다 크고 1000보다 작은 숫자 이군요.\n", num);
+            } else {
+                System.out.printf("입력된 숫자 %d는(은) 1000보다 큰 숫자 이군요.\n", num);
+            }
+        } else {
+            System.out.printf("입력된 숫자 %d는(은) 100보다 작은 숫자 이군요.\n", num);
+        }
+
+        s.close();
+    }
+}
